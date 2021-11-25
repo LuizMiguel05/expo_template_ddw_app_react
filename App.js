b24c9fe1b4a0e248e64b9b0bd400d8e802fcbc9f
LuizMiguel05 <josehenrique2362@gmail.com>
2021-11-25 17:46:06 +0000

    segunda aplicação

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacityBase, View } from 'react-native';
+
+function enviar(){
+    alert('Você clicou no notão!');
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Minha primeira aplicação React!!!</Text>
+      <Text style={styles.titulo}>Segundo Aplicativo</Text>
+      <textInput style={styles.campo}></textInput>
+      <TouchableOpacity style={styles.botao}>
+        <text style={styles.textobotao}>Mostrar</text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+nt
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+
+titulo:{
+    fontSize: 30,
+    textAlign: 'center',
+    marginTop: 15,
+    color: '#800000'
+},
+
+campo:{
+    backgroundColor:'#FFA07A', 
+    fontSize: 20,
+    margin: 15,
+    padding:  10,
+    color: '#FFF',
+    borderRadius: 20
+},
+
+botao:{
+    backgroundColor: '#800000',
+    margin: 15,
+    alignItems: ' center',
+    padding: 10, 
+    borderRadius: 20
+},
+
+textobotao:{
+    color: 'FFF',
+    fontSize: 25
+}
 });
